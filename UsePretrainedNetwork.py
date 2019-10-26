e4ff5b26c72c404d6884554d489062a683c40fb9
Newjoy <38489219+newjoy2018@users.noreply.github.com>
2019-10-26 16:15:58 +0200

    add figure about accuracy and loss

--- UsePretrainedNetwork.py
+++ UsePretrainedNetwork.py
@@ -64,3 +64,27 @@ history = model.fit(train_features, train_labels,
                    batch_size=20,
                    validation_data=(validation_features, validation_labels))
 #----------------------------------------------------------------------------------
+#-----绘制Accuracy和Loss图---------------------------------------------------------
+#----------------------------------------------------------------------------------
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc)+1)
+
+plt.plot(epochs, acc, 'bo', label='Trainging acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Trainging and Validation Accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Trainging and Validation Loss')
+plt.legend()
+
+plt.show()
