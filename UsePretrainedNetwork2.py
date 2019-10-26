28ee6387f10649b83162987a4f04e736131e1530
Newjoy <38489219+newjoy2018@users.noreply.github.com>
2019-10-26 17:28:34 +0200

    Update UsePretrainedNetwork2.py

--- UsePretrainedNetwork2.py
+++ UsePretrainedNetwork2.py
@@ -8,6 +8,8 @@ conv_base = VGG16(weights='imagenet',
                  include_top=False,
                  input_shape=(150,150,3))
 #----------------------------------------------------------------------------
+conv_base.trainable = False
+
 from keras import layers
 from keras import models
 
@@ -19,3 +21,42 @@ model.add(layers.Dense(1, activation='sigmoid'))
 
 model.summary()
 #----------------------------------------------------------------------------
+import os
+base_dir = 'D:/data/cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                  rotation_range=40,
+                                  width_shift_range=0.2,
+                                  height_shift_range=0.2,
+                                  shear_range=0.2,
+                                  zoom_range=0.2,
+                                  horizontal_flip=True,
+                                  fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                   target_size=(150, 150),
+                                                   batch_size=20,
+                                                   class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(validation_dir,
+                                                               target_size=(150, 150),
+                                                               batch_size=20,
+                                                               class_mode='binary')
+model.compile(loss='binary_crossentropy',
+             optimizer=optimizers.RMSprop(lr=2e-5),
+             metrics=['acc'])
+
+#------------------------------------------------------------------------------
+history = model.fit_generator(train_generator,
+                             steps_per_epoch=100,
+                             epochs=30,
+                             validation_data=validation_generator,
+                             validation_steps=50)
