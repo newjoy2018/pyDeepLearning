ad25df4454dd5e36a77106c0aa05ca4fbb79978f
Newjoy <38489219+newjoy2018@users.noreply.github.com>
2019-10-27 11:33:56 +0100

    add plot and test and visualization

--- selfDefinedModel_1.py
+++ selfDefinedModel_1.py
@@ -57,3 +57,66 @@ history = model.fit_generator(train_generator,
                              epochs=40,
                              validation_data=validation_generator,
                              validation_steps=50)
+
+#-----plot acc/loss figure---------------------------------------------------------
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc)+1)
+
+plt.plot(epochs, acc, 'bo', label='Trainging acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Trainging and Validation Accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Trainging and Validation Loss')
+plt.legend()
+
+plt.show()
+#-----------------------------------------------------------------------------------
+#-----test the Accuracy in new data(test_dir)---------------------------------------
+test_generator = test_datagen.flow_from_directory(test_dir,
+                                                 target_size=(150,150),
+                                                 batch_size=20,
+                                                 class_mode='binary')
+
+test_loss, test_accuracy = model.evaluate_generator(test_generator, steps=50)
+print('test_acc:', test_accuracy)
+#-----------------------------------------------------------------------------------
+#-----Visualization-----------------------------------------------------------------
+img_path = 'D:/data/cats_and_dogs_small/test/cats/cat.1702.jpg'
+
+from keras.preprocessing import image
+import numpy as np
+
+img = image.load_img(img_path, target_size=(150, 150))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+img_tensor /= 255.
+
+print(img_tensor.shape)
+
+import matplotlib.pyplot as plt
+plt.imshow(img_tensor[0])
+plt.show()
+
+from keras import models
+
+layer_outputs = [layer.output for layer in model.layers[:2]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+
+activations = activation_model.predict(img_tensor)
+first_layer_activation = activations[0]
+print(first_layer_activation.shape)
+
+import matplotlib.pyplot as plt
+plt.matshow(first_layer_activation[0,:,:,4], cmap='viridis')
+#-----------------------------------------------------------------------------------
